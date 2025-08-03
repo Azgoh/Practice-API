6763cb24b76933d186290335d2257c7f3c7a6ed4
NikolasTsm <ntsamitr@ece.auth.gr>
2025-08-04 02:18:19 +0300

    Add OAuth2 exception

--- src/main/java/com/example/PracticeApi/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/com/example/PracticeApi/exception/OAuth2AuthenticationProcessingException.java
@@ -1,4 +1,11 @@
 package com.example.PracticeApi.exception;
 
-public class OAuth2AuthenticationProcessingException {
+import org.springframework.security.core.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+
+    public OAuth2AuthenticationProcessingException(String msg){
+        super(msg);
+    }
+
 }
